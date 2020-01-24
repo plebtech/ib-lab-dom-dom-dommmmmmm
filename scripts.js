1592e432a0783a773bfedd898daf1267a5d3996d
Nick Mc <nbmc@protonmail.com>
2020-01-24 09:23:59 -0600

    added unique id assignment to black boxes.

--- scripts.js
+++ scripts.js
@@ -1,28 +1,37 @@
+// listener for DOM content loaded.
+// browser fully loads HTML, then script executes.
 document.addEventListener("DOMContentLoaded", onLoad());
 
+// function called by DOMContentLoaded listener.
 function onLoad() {
     makeAddSquareButton();
+    makeFlexContainer();
 }
 
+// makes button, adds listener to it.
 function makeAddSquareButton() {
     let addSquareButton = document.createElement('button');
     let addSquareButtonText = document.createTextNode("Add Square");
     addSquareButton.appendChild(addSquareButtonText);
     document.body.appendChild(addSquareButton);
-    makeFlexContainer();
     listenAddSquare(addSquareButton);
 }
 
+// makes flex container for black boxes.
 function makeFlexContainer() {
     let flexContainer = document.createElement('div');
     flexContainer.classList.add('flex-container');
     document.body.appendChild(flexContainer);
 }
 
+// makes black boxes.
 function listenAddSquare(btn) {
+    let blackBoxCounter = 1;
     btn.addEventListener("click", function () {
         let divBlackSquare = document.createElement('div');
         divBlackSquare.classList.add('black-square');
+        divBlackSquare.id = "bs" + blackBoxCounter;
+        blackBoxCounter++;
         document.querySelector('.flex-container').appendChild(divBlackSquare);
     });
 }
