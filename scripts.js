2968ce4325c12fa78509a15dbf09f60823a65c44
Nick Mc <nbmc@protonmail.com>
2020-01-24 14:08:01 -0600

    cleaned up code (removed extraneous comments.

--- scripts.js
+++ scripts.js
@@ -4,7 +4,6 @@ document.addEventListener('DOMContentLoaded', onLoad());
 
 // function called by DOMContentLoaded listener.
 function onLoad() {
-    // makeAlertDiv();
     makeAddSquareButton();
     makeFlexContainer();
 }
