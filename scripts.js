196ed0542a61046f2fdbc7ad78d526c87839db7c
Nick Mc <nbmc@protonmail.com>
2020-01-24 09:14:11 -0600

    added functionality to 'add square' button.

--- scripts.js
+++ scripts.js
@@ -9,4 +9,20 @@ function makeAddSquareButton() {
     let addSquareButtonText = document.createTextNode("Add Square");
     addSquareButton.appendChild(addSquareButtonText);
     document.body.appendChild(addSquareButton);
+    makeFlexContainer();
+    listenAddSquare(addSquareButton);
+}
+
+function makeFlexContainer() {
+    let flexContainer = document.createElement('div');
+    flexContainer.classList.add('flex-container');
+    document.body.appendChild(flexContainer);
+}
+
+function listenAddSquare(btn) {
+    btn.addEventListener("click", function () {
+        let divBlackSquare = document.createElement('div');
+        divBlackSquare.classList.add('black-square');
+        document.querySelector('.flex-container').appendChild(divBlackSquare);
+    });
 }
