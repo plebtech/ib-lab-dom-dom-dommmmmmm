d5f37d85c8e78db937c40513cfcf97862d936e42
Nick Mc <nbmc@protonmail.com>
2020-01-24 13:59:34 -0600

    completed lab with doubleclick functionality.

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', onLoad());
 
 // function called by DOMContentLoaded listener.
 function onLoad() {
+    // makeAlertDiv();
     makeAddSquareButton();
     makeFlexContainer();
 }
@@ -30,11 +31,21 @@ function listenAddSquare(btn) {
     btn.addEventListener('click', function () {
         let divBlackSquare = document.createElement('div');
         divBlackSquare.classList.add('black-square');
+        blackBoxCounter = updateCounter();
         divBlackSquare.id = blackBoxCounter;
-        blackBoxCounter++;
         document.querySelector('.flex-container').appendChild(divBlackSquare);
         listenSquare(divBlackSquare);
     });
+}
+
+// update the counter for number of boxes (boxes existent ON button click).
+function updateCounter() {
+    let boxes = document.querySelectorAll('.black-square');
+    let count = 0;
+    boxes.forEach(function (box) {
+        count++;
+    })
+    return count;
 }
 
 // random color function.
@@ -43,16 +54,36 @@ function randomColor(e) {
     return colors[Math.floor(Math.random() * colors.length)];
 }
 
-
+// collection of all listeners for the black squares.
 function listenSquare(square) {
-    square.addEventListener('mouseover', function() {
+    square.addEventListener('mouseover', function () {
         let id = document.createTextNode(square.id);
         square.textContent = square.id;
     });
-    square.addEventListener('mouseout', function() {
+    square.addEventListener('mouseout', function () {
         square.textContent = '';
     });
-    square.addEventListener('click', function() {
+    square.addEventListener('click', function () {
         square.style.backgroundColor = randomColor();
     });
+    square.addEventListener('dblclick', function () {
+        doubleClickedSquare(square);
+    });
+}
+
+// complex logic for double click.
+function doubleClickedSquare(square) {
+    if ((square.id % 2) === 0) {
+        if (square.nextSibling === null) {
+            alert("The box is even, yet no box exists to its right.");
+        } else {
+            square.nextSibling.remove();
+        }
+    } else if ((square.id % 2) !== 0) {
+        if (square.previousSibling === null) {
+            alert("The box is odd, yet no box exists to its left.");
+        } else {
+            square.previousSibling.remove();
+        }
+    }
 }
